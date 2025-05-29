8a7802a590aa12a5cccfdf489d34b243fd55e69a
박민주 <pmju5436@gmail.com>
2025-05-29 20:16:36 +0900

    feat: show live streams from server

--- apps/web/src/pages/explore/livecard/livecard.tsx
+++ apps/web/src/pages/explore/livecard/livecard.tsx
@@ -2,16 +2,24 @@ import { useNavigate } from "react-router-dom";
 import {LiveCardProps} from "./livecard.props.ts";
 import {Thumbnail, LiveCardContainer, LiveInfo, LiveTitle, ProfileImage, StreamerName, TitleContainer, Spacer} from "./livecard.style.ts";
 
-export const LiveCard = ({ profileImage, title, streamerName }: LiveCardProps) => {
+export const LiveCard = ({ streamImage, profileImage, title, nickname }: LiveCardProps) => {
+
+    const navigate = useNavigate();
+
+    const goToStream = () => {
+        navigate(`/livedetail`);
+        // ?url=${encodeURIComponent(streamUrl)}
+    }
+
     return (
-        <LiveCardContainer>
-            <Thumbnail src={profileImage}/>
+        <LiveCardContainer onClick={goToStream}>
+            <Thumbnail src={streamImage}/>
                 <Spacer />
             <LiveInfo>
                 <ProfileImage src={profileImage}/>
                 <TitleContainer>
                     <LiveTitle>{title}</LiveTitle>
-                    <StreamerName>{streamerName}</StreamerName>
+                    <StreamerName>{nickname}</StreamerName>
                 </TitleContainer>
             </LiveInfo>
         </LiveCardContainer>
--- apps/web/src/pages/explore/livecard/mainlivecard/mainlivecard.tsx
+++ apps/web/src/pages/explore/livecard/mainlivecard/mainlivecard.tsx
@@ -1,17 +1,17 @@
-import { MainLiveProps } from "./mainlivecard.props"
+// import { MainLiveProps } from "./mainlivecard.props"
 import { BadgeStatusContainer, CategoryText, CheckBadge, FollowCount, LiveChip, LiveChipText, LiveStatusContainer, MainLiveContainer, MainStreamerName, StreamInfo, ViewCount } from "./mainlivecard.style"
 import Badge from '../../../../assets/badge.png'
 import { ProfileImage, Spacer,  } from "../livecard.style"
 import {  useNavigate } from "react-router-dom"
+import {LiveStream} from "../../../main/main.tsx";
 export const MainLiveCard = ({
-    thumbnail,
+    nickname,
     profileImage,
-    streamerName,
-    followerCount,
-    viewerCount,
-    category,
-    badge
-}:MainLiveProps) => {
+    badgeImage,
+    // title,
+    streamImage,
+    // streamUrl
+}:LiveStream) => {
 
     const navigate = useNavigate();
 
@@ -20,23 +20,23 @@ export const MainLiveCard = ({
     }
 
     return (
-            <MainLiveContainer thumbnail={thumbnail} onClick={clickLive}>
+            <MainLiveContainer thumbnail={streamImage} onClick={clickLive}>
                 <LiveStatusContainer>
                     <LiveChip>
                         <LiveChipText>LIVE</LiveChipText>
                     </LiveChip>
-                    <ViewCount>{viewerCount}명 시청중</ViewCount>
+                    <ViewCount>{/*viewerCount*/0}명 시청중</ViewCount>
                 </LiveStatusContainer>
-                <CategoryText>{category}</CategoryText>
+                <CategoryText>{/*category*/"버츄얼"}</CategoryText>
                 <Spacer />
                 <StreamInfo>
                 <ProfileImage src={profileImage}/>
                 <div style={{display:'flex', flexDirection: 'column', marginBottom: 10}}>
                 <BadgeStatusContainer>
-                    <MainStreamerName>{streamerName}</MainStreamerName>
-                    {badge && <CheckBadge src={Badge} />}
+                    <MainStreamerName>{nickname}</MainStreamerName>
+                    {badgeImage && <CheckBadge src={Badge} />}
                 </BadgeStatusContainer>
-                <FollowCount>팔로워 {followerCount}명</FollowCount>
+                <FollowCount>팔로워 {/*followerCount*/0}명</FollowCount>
                 </div>
                 </StreamInfo>
             </MainLiveContainer>
