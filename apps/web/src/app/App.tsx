93b8596aa3ea8a1777b4cd618ac9bd4401cc3a4f
Ingrsso <79688235+eunsdev@users.noreply.github.com>
2025-08-29 08:57:21 +0900

    feat: Support import @/

--- apps/web/src/app/App.tsx
+++ apps/web/src/app/App.tsx
@@ -1,11 +1,9 @@
 import { useState } from 'react'
 import reactLogo from './assets/react.svg'
 import viteLogo from '/vite.svg'
-import styled from 'styled-components'
 
 function App() {
   const [count, setCount] = useState(0)
-
   return (
     <>
       <div>
