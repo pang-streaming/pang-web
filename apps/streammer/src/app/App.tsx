495a1d96343e873e4f96a11ad7b3d6bf36879b2a
kmg <legolove08@naver.com>
2025-08-29 15:20:16 +0900

    feat: base theme

--- apps/streammer/src/app/App.tsx
+++ apps/streammer/src/app/App.tsx
@@ -1,36 +1,24 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import styled from 'styled-components'
+// import {SegmentButton} from "@pang/shared/ui/buttons/segmentButton.tsx";
+import styled from "styled-components";
+import {CustomThemeProvider} from "@pang/shared/shared";
+
+
+
+
+
+const TestContainer = styled.div`
+    width: 100px;
+    height: 100px;
+    background-color: ${({theme}) => theme.colors.primary.normal};
+`
 
 function App() {
-  const [count, setCount] = useState(0)
-
   return (
-    <>
-      <div>
-        <a href="https://vite.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.tsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>      
-    </>
+      <CustomThemeProvider>
+        <TestContainer />
+        {/*<SegmentButton tabs={["Home", "About"]} onClick={(tabs) => {console.log(tabs);}} />*/}
+      </CustomThemeProvider>
   )
 }
-
 
 export default App
--- apps/streammer/src/app/provider.tsx
+++ apps/streammer/src/app/provider.tsx
@@ -1,11 +1,13 @@
 import { QueryClientProvider } from "@tanstack/react-query"
 import { queryClient } from "../shared/lib/query-client"
 import { ThemeProvider } from "styled-components"
-import { theme } from "@pang/shared/ui"
+import {lightTheme} from "@pang/shared/theme";
+
+
 
 export const Provider = ({children}: {children: React.ReactNode}) => {
     return <QueryClientProvider client={queryClient}>
-        <ThemeProvider theme={theme}>
+        <ThemeProvider theme={lightTheme}>
             {children}
         </ThemeProvider>
     </QueryClientProvider>
