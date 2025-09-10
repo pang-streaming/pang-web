d31ceacbe33f00c6a1662b75683d45a61da9f2f8
jiyun0220 <145185712+jiyun0220@users.noreply.github.com>
2025-09-10 11:49:53 +0900

    feat: router

---
+++ apps/streammer/src/pages/revenue/ui/index.tsx
@@ -0,0 +1,9 @@
+import React from 'react';
+
+const RevenuePage = () => {
+  return (
+      <></>
+  );
+};
+
+export default RevenuePage;
---
+++ apps/streammer/src/pages/streaming/index.ts
@@ -0,0 +1,1 @@
+export { default as StreamingPage } from './ui';
