e7decefeca7775d93d960214319b1f832e4161f7
박민주 <pmju5436@gmail.com>
2025-10-18 01:07:06 +0900

    feat: 스트리머 헤더

--- shared/src/ui/header/header.tsx
+++ shared/src/ui/header/header.tsx
@@ -17,71 +17,73 @@ interface HeaderProps {
 }
 
 export const Header = ({ onClickMenu, type }: HeaderProps) => {
-	const navigate = useNavigate();
-	const { mode, toggleTheme } = useThemeStore();
-	const DarkLightModeIcon = mode === "dark" ? PiSunBold : HiOutlineMoon;
-	const MoveButton = type === "user" ? FiVideo : PiMonitorBold;
+  const navigate = useNavigate();
+  const { mode, toggleTheme } = useThemeStore();
+  const DarkLightModeIcon = mode === "dark" ? PiSunBold : HiOutlineMoon;
+  const MoveButton = type === "user" ? FiVideo : PiMonitorBold;
 
-	const token = localStorage.getItem("accessToken");
-	const isLoggedIn = token == null;
+  const token = localStorage.getItem("accessToken");
+  const isLoggedIn = token == null;
 
-
-	return (
-		<HeaderContainer>
-			<LogoWrapper>
-				<SidebarToggleButton size={28} onClick={onClickMenu}/>
-				<PangLogo type={type} onClick={() => navigate('/')} cursor="pointer"/>
-			</LogoWrapper>
-			{type === "user" && <SearchBar/>}
-			<ButtonWrapper>
-				<HeaderButton Icon={MoveButton}/>
-				<HeaderButton Icon={HiOutlineBell}/>
-				<HeaderButton Icon={DarkLightModeIcon} onClick={toggleTheme}/>
-				<LoginButton isLoggedIn={isLoggedIn}/>
-			</ButtonWrapper>
-		</HeaderContainer>
-	)
-}
+  return (
+    <HeaderContainer>
+      <LogoWrapper>
+        {/* 스트리머 타입이 아닐 때만 메뉴 아이콘 렌더링 */}
+        {type !== "streamer" && (
+          <SidebarToggleButton size={28} onClick={onClickMenu} />
+        )}
+        <PangLogo type={type} onClick={() => navigate("/")} cursor="pointer" />
+      </LogoWrapper>
+      {type === "user" && <SearchBar />}
+      <ButtonWrapper>
+        <HeaderButton Icon={MoveButton} />
+        <HeaderButton Icon={HiOutlineBell} />
+        <HeaderButton Icon={DarkLightModeIcon} onClick={toggleTheme} />
+        <LoginButton isLoggedIn={isLoggedIn} />
+      </ButtonWrapper>
+    </HeaderContainer>
+  );
+};
 
 const LogoWrapper = styled.div`
-	display: flex;
-	flex-direction: row;
-	align-items: center;
-	justify-content: center;
-	gap: 24px;
-	margin-right: 36px;
+  display: flex;
+  flex-direction: row;
+  align-items: center;
+  justify-content: center;
+  gap: 24px;
+  margin-right: 36px;
 `;
 
 const ButtonWrapper = styled.div`
-    display: flex;
-    flex-direction: row;
-    align-items: center;
-    justify-content: center;
-    gap: 18px;
-`
+  display: flex;
+  flex-direction: row;
+  align-items: center;
+  justify-content: center;
+  gap: 18px;
+`;
 
 const SidebarToggleButton = styled(IoMenu)`
-    cursor: pointer;
-	padding: 6px;
-	border-radius: ${({ theme }) => theme.borders.large};
-    color: ${({ theme }) => theme.colors.button.active};
+  cursor: pointer;
+  padding: 6px;
+  border-radius: ${({ theme }) => theme.borders.large};
+  color: ${({ theme }) => theme.colors.button.active};
 
-	&:hover {
-	background-color: ${({ theme }) => theme.colors.hover.light};
-	}
+  &:hover {
+    background-color: ${({ theme }) => theme.colors.hover.light};
+  }
 `;
 
 const HeaderContainer = styled.header`
-	position: fixed;
-	top: 0;
-	left: 0;
-	right: 0;
-	height: 66px;
-	padding: 0 22px;
-	background-color: ${({ theme }) => theme.colors.background.normal};
-	border-bottom: 1px solid ${({ theme }) => theme.colors.border.light};
-	display: flex;
-	align-items: center;
-	justify-content: space-between;
-	z-index: 10;
+  position: fixed;
+  top: 0;
+  left: 0;
+  right: 0;
+  height: 66px;
+  padding: 0 22px;
+  background-color: ${({ theme }) => theme.colors.background.normal};
+  border-bottom: 1px solid ${({ theme }) => theme.colors.border.light};
+  display: flex;
+  align-items: center;
+  justify-content: space-between;
+  z-index: 10;
 `;
